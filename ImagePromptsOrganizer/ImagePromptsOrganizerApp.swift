20c7f9f855e361d5c602cb2ca2132b33ab7636e6
Aashish Patil <aashish.mobile.apps@gmail.com>
2025-10-04 10:54:39 -0700

    Initial Commit

---
+++ ImagePromptsOrganizer/ImagePromptsOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePromptsOrganizerApp.swift
+//  ImagePromptsOrganizer
+//
+//  Created by Aashish Patil on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ImagePromptsOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
